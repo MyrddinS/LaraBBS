fab2368489e7fbd0b7a79e480795677433addb34
MyrddinS <sunweian@hotmail.com>
2020-11-05 11:51:10 +0000

    邮箱认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,11 +37,12 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailIsVerified::class,
         ],
 
         'api' => [
             'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'bindings',
         ],
     ];
 
